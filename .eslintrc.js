232c4d921622518838fdb8b05d71257ea5d2046f
Yudai NAKATA <yudai.nkt@gmail.com>
2022-01-23 00:48:48 +0900

    chore: introduce eslint-plugin-eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@
 /** @type import("@typescript-eslint/utils").TSESLint.Linter.Config */
 module.exports = {
   extends: ["@yudai-nkt"],
+  plugins: ["eslint-plugin"],
   overrides: [
     {
       files: ["*.ts"],
@@ -9,6 +10,14 @@ module.exports = {
       // Some errors cannot be resolved even with a DeepReadonly<T> utility,
       // so disable this rule temporarily until an investigation is made.
       rules: { "@typescript-eslint/prefer-readonly-parameter-types": "off" },
+    },
+    {
+      files: ["src/rules/*.ts"],
+      extends: ["plugin:eslint-plugin/rules-recommended"],
+    },
+    {
+      files: ["src/__tests__/rules/*.ts"],
+      extends: ["plugin:eslint-plugin/tests-recommended"],
     },
   ],
 };
